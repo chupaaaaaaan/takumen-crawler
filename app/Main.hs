7dfb80f9d7114566ae8b4b20f74c391dfdacc740
Takayuki Uchida <jacobitebeerlove@gmail.com>
2020-06-13 17:06:03 +0900

    slackに情報を飛ばせるようになるまで作成

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,110 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
 
-import           Network.HTTP.Client
-import           Text.HTML.Scalpel
-import           Data.Maybe
-import           Data.Text                      ( Text )
-import qualified Data.Text                     as T
-import qualified Data.Text.IO                  as T
-import qualified Data.Text.ICU                 as TI
-import Control.Monad
-import Control.Monad.Reader 
-import Control.Monad.Trans.Reader hiding (ask, asks)
+import Network.HTTP.Client
+import Text.HTML.Scalpel
+import Data.Aeson
+import Data.Text (Text)
+import Data.Maybe
 import Data.IORef
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Control.Monad.Reader
+import Control.Concurrent
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import GHC.Generics (Generic)
+import Control.Monad.Cont
+import qualified Web.Slack as Slack
+import qualified Web.Slack.Chat as Slack
+import qualified Web.Slack.Common as Slack
+import System.Environment
+import System.Random.MWC
 
 type Name = Text
 type Link = Text
 type Description = Text
 type Price = Text
-data ItemState = SoldOut | Available
+data ItemState = SoldOut | Available deriving (Show, Eq, Generic)
+instance ToJSON ItemState
+instance FromJSON ItemState
 
 data Item = Item { name        :: Name
                  , link        :: Link
                  , description :: Description
                  , price       :: Price
                  , itemState   :: ItemState
-                 }
+                 } deriving (Show, Eq, Generic)
+
+instance ToJSON Item
+instance FromJSON Item
+
+data UrlConfig = UrlConfig { baseUrl :: Text
+                           , searchUrl :: Text
+                           }
 
 prettyPrint :: Maybe [Item] -> IO ()
 prettyPrint Nothing = T.putStrLn "No Content"
-prettyPrint (Just items) = forM_ items $ \Item{..} -> do
+prettyPrint (Just item) = forM_ item $ \Item{..} ->
   case itemState of
-    SoldOut   -> T.putStrLn $ "{ " <> name <> "\n, " <> link <> "\n, " <> rmLn description <> "\n, " <> rmLn price <> "\n, " <> "SoldOut"   <> "\n} "
-    Available -> T.putStrLn $ "{ " <> name <> "\n, " <> link <> "\n, " <> rmLn description <> "\n, " <> rmLn price <> "\n, " <> "Available" <> "\n} "
-  where rmLn = T.concat . T.lines
+    SoldOut   -> T.putStrLn $ "{ " <> name <> "\n, " <> link <> "\n, " <> description <> "\n, " <> price <> "\n, " <> "SoldOut"   <> "\n} "
+    Available -> T.putStrLn $ "{ " <> name <> "\n, " <> link <> "\n, " <> description <> "\n, " <> price <> "\n, " <> "Available" <> "\n} "
 
 
-data Env = Env { store :: IORef Text
+-- scraper
+items :: Scraper Text [Item]
+items = chroots ("div" @: ["id" @= "products"] // "div" @: [hasClass "inner", hasClass "clearfix"]) scrapeItem
+
+scrapeItem :: Scraper Text Item
+scrapeItem = do
+  n <- scrapeName
+  l <- scrapeLink
+  d <- scrapeDesc
+  p <- scrapePrice
+  s <- scrapeItemState
+  return $ Item n l d p s
+
+scrapeName :: Scraper Text Name
+scrapeName = text $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "pLink"] // "a"
+
+scrapeLink :: Scraper Text Link
+scrapeLink = attr "href" $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "pLink"] // "a"
+
+scrapeDesc :: Scraper Text Description
+scrapeDesc = do
+  d <- text $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "title"]
+  return . T.concat . T.lines $ d
+
+scrapePrice :: Scraper Text Price
+scrapePrice = do
+  p <- text $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "price"]
+  return . T.concat . T.lines $ p
+
+scrapeItemState :: Scraper Text ItemState
+scrapeItemState = do
+  let soldOutImgUrl = "/assets/btn_soldout_mini.jpg"
+  s <- attr "src"  $ "div" @: [hasClass "checkBox"] // "img"
+  return $ if s == soldOutImgUrl then SoldOut else Available
+
+
+-- app framework
+data Env = Env { store :: IORef (Map Link Item)
                , manager :: Manager
+               , urlConfig :: UrlConfig
+               , slackConfig :: Slack.SlackConfig
+               , slackTargetChannel :: Text
                }
 
--- newtype ContT r m a = ContT { runContT :: (a -> m r) -> m r }
+instance Slack.HasManager Env where
+  getManager = Slack.slackConfigManager . slackConfig
+
+instance Slack.HasToken Env where
+  getToken = Slack.slackConfigToken . slackConfig
 
 newtype App a = App (ReaderT Env IO a)
   deriving( Functor
@@ -54,42 +117,50 @@ newtype App a = App (ReaderT Env IO a)
 runApp :: Env -> App a -> IO a
 runApp env (App m) = runReaderT m env
 
-baseUrl = "https://www.takumen.com"
-searchUrl = baseUrl <> "/search/index?all=true"
-soldOutImgUrl = "/assets/btn_soldout_mini.jpg"
-
-items :: Scraper Text [Item]
-items = chroots ("div" @: ["id" @= "products"] // "div" @: [hasClass "inner", hasClass "clearfix"]) $ do
-  n  <- text        $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "pLink"] // "a"
-  l  <- attr "href" $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "pLink"] // "a"
-  d  <- text        $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "title"]
-  p  <- text        $ "div" @: [hasClass "textBox"]  // "p" @: [hasClass "price"]
-  s' <- attr "src"  $ "div" @: [hasClass "checkBox"] // "img"
-
-  let s = if s' == soldOutImgUrl then SoldOut else Available
-  return $ Item n l d p s
 
 
+postAvailableTakumen :: [Item] -> App ()
+postAvailableTakumen is = do
+  storeRef <- asks store
+  oldMap   <- liftIO $ readIORef storeRef
+  channel  <- asks slackTargetChannel
+  urls     <- asks urlConfig
+  igo <- forM is $ \item ->
+    return $ if itemState item == SoldOut
+             then Nothing
+             else case M.lookup (link item) oldMap of
+                    Nothing -> Just item
+                    Just oi -> if itemState oi == SoldOut then Just item else Nothing
+
+  let msg = foldMap ((\i -> name i <> "\n" <> baseUrl urls <> link i <> "\n\n") . fromJust) . filter (/=Nothing) $ igo
+      request = Slack.mkPostMsgReq channel msg
+  Slack.chatPostMessage request
+
+  liftIO $ writeIORef storeRef $ M.fromList $ map (\i -> (link i, i)) is
 
 app :: App ()
 app = do
-  ref <- asks store
-  manager <- liftIO $ newManager defaultManagerSettings
-  liftIO $ scrapeURLWithConfig (Config utf8Decoder Nothing) searchUrl items >>= prettyPrint
-  -- forever $ do
-  --   result <- liftIO $ scrapeURLWithConfig (Config utf8Decoder Nothing) searchUrl items
-  --   case result of
-  --     Nothing -> return ()
-  --     Just x -> do
-  --       old <- liftIO $ readIORef ref
+  -- gen <- liftIO $ createSystemRandom
+  urls <- asks urlConfig
+  forever $ do
+    scraped <- liftIO $ scrapeURLWithConfig (Config utf8Decoder Nothing) (T.unpack $ baseUrl urls <> searchUrl urls) items
 
-  --       return ()
-  
-  return ()
+    liftIO $ prettyPrint scraped
 
+    case scraped of
+      Nothing -> error "結果の取得に失敗しました。"
+      Just result -> postAvailableTakumen result
+
+    liftIO $ threadDelay (60 * 1000000)
 
 main :: IO ()
 main = do
-  s <- newIORef T.empty
-  m <- newManager defaultManagerSettings
-  runApp (Env s m) app
+  [slackToken, slackTargetChannel'] <- getArgs
+  storeRef <- newIORef M.empty
+  manager' <- newManager defaultManagerSettings
+  slackConfig' <- Slack.mkSlackConfig $ T.pack slackToken
+
+  let urlConfig' = UrlConfig "https://www.takumen.com" "/search/index?all=true"
+      env = Env storeRef manager' urlConfig' slackConfig' (T.pack slackTargetChannel')
+
+  runApp env app
